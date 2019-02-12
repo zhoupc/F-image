163909bd69e88e6f5335cfc9f791ee05b1716c61
Pengcheng Zhou <zhoupc1988@gmail.com>
2019-02-12 18:27:47 -0500

    update fi.selfupdate

--- +fi/selfupdate.m
+++ +fi/selfupdate.m
@@ -5,16 +5,7 @@ try
     cd(fi.home_dir);
     system('git pull');
     fprintf('F-image:\nupdated.\n\tpath:%s\n', fi.home_dir());
-<<<<<<< HEAD
-catch
-    tmp_folder = fi.home_dir; 
-    unzip('https://github.com/zhoupc/F-image/archive/master.zip');  % not git installed
-    movefile('F-image-master', tmp_folder)
-end
-success = true; 
-=======
     success = true;
 catch
     success = false;
 end
->>>>>>> 2913a7810d46f77f0da2e1b13ec800880e0fbddd
